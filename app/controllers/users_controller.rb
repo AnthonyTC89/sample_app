ee42c583acb54f1663161741d18131cd50f27b78
AnthonyTC89 <ptonyptc@gmail.com>
2019-09-24 16:31:31 -0500

    Unsuccessful edits done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,6 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def edit
-    @user = User.find(params[:id])
-  end
-  
   def show
     @user = User.find(params[:id])
   end
@@ -20,6 +16,19 @@ class UsersController < ApplicationController
       redirect_to @user
     else
       render 'new'
+    end
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+    else
+      render 'edit'
     end
   end
   private
