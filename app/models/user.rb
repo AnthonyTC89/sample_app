3e0ad14e68b27f1820a3101d6ed7f978000e5048
AnthonyTC89 <ptonyptc@gmail.com>
2019-09-24 13:00:13 -0500

    Two subtle bugs done

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,8 +30,10 @@ class User < ApplicationRecord
   def forget
     update_attribute(:remember_digest, nil)
   end
-  
+
+  # Returns true if the given token matches the digest.
   def authenticated?(remember_token)
+    return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 end
